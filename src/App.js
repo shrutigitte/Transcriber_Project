384a555e1d554970be9a8f4f4db1916787e2af5a
Devesh0403 <97463053+Devesh0403@users.noreply.github.com>
2024-05-07 23:11:41 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import Navbar from "./components/navbar"
+import Main from "./components/main"
+
+function App() {
+  return (
+    <>
+
+    <Navbar/>
+    <Main />
+    
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/main.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./main.css"
+
+const Main = () => {
+    return (
+        <div class="main_component">
+
+        <h1>Get a YouTube Transcript For Free</h1>
+        <h3>Instantly, without uploading any files!</h3>
+
+        <form>
+        
+        <input type="text" placeholder='Enter YouTube link'/>
+        <button type="button">Get transcript!</button>
+
+        </form>
+        </div>
+    
+)
+}
+
+export default Main
